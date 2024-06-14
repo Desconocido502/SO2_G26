bbf137b76d08ef85046c4cca7d9efe0a40aa5c74
Carlos Soto <3018554470101@ingenieria.usac.edu.gt>
2024-06-14 00:44:19 -0600

    SO2_PR2 #comment UPDATE: Se agrego la correcta lectura y reporte de la carga de usuarios #time 45m

--- practica_2/main.c
+++ practica_2/main.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cjson/cJSON.h>
 #include <string.h>
-#include "users.h"
-#include <stdbool.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <stdbool.h>
-#include <regex.h>
 #include <time.h>
+#include <cjson/cJSON.h>
 
-#define THREADS_USERS 3
-#define THREADS_OPERATIONS 4
-#define MAX_LINE_LENGTH 1000
 #define MAX_USERS 500
-#define MAX_TRANSACIONES 220
 #define MAX_ERRORS 100
 
 struct user_data {
@@ -25,22 +15,6 @@ struct user_data {
     int line; // Para almacenar el número de línea del JSON
 };
 
-struct operation {
-    int operation;
-    int account_1;
-    int account_2;
-    float monto;
-};
-
-typedef struct {
-    int thread_id;
-    int start_index;
-    int end_index;
-    int errors;
-    double work_time;
-    double lock_time;
-} ThreadData;
-
 struct error_data {
     int line;
     char error_message[200];
@@ -49,79 +23,16 @@ struct error_data {
 struct error_data errors[MAX_ERRORS];
 int num_errors = 0;
 
-pthread_mutex_t mutex;
 struct user_data users[MAX_USERS];
-int num_users;
+int num_users = 0;
 
-void* insert_users(void* data) {
-    ThreadData* thread_data = (ThreadData*)data;
-    int start = thread_data->start_index;
-    int end = thread_data->end_index;
-    int errors = 0;
+pthread_mutex_t mutex;
+// Variables para contar usuarios cargados por cada hilo
+int usuariosHilo1 = 0;
+int usuariosHilo2 = 0;
+int usuariosHilo3 = 0;
 
-    clock_t work_start, work_end;
-    clock_t lock_start, lock_end;
-    double total_work_time = 0.0;
-    double total_lock_time = 0.0;
-
-    for (int i = start; i < end; ++i) {
-        work_start = clock();
-
-        lock_start = clock();
-        pthread_mutex_lock(&mutex);
-        lock_end = clock();
-
-        insertUser(users[i].account_number, users[i].full_name, users[i].saldo);
-        pthread_mutex_unlock(&mutex);
-
-        work_end = clock();
-
-        total_work_time += (double)(work_end - work_start) / CLOCKS_PER_SEC * 1000;
-        total_lock_time += (double)(lock_end - lock_start) / CLOCKS_PER_SEC * 1000;
-    }
-
-    thread_data->errors = errors;
-    thread_data->work_time = total_work_time;
-    thread_data->lock_time = total_lock_time;
-
-    pthread_exit(NULL);
-}
-
-void execute_concurrent_user_insertions() {
-    pthread_t threads[THREADS_USERS];
-    ThreadData thread_data[THREADS_USERS];
-    int users_per_thread = num_users / THREADS_USERS;
-
-    for (int i = 0; i < THREADS_USERS; ++i) {
-        thread_data[i].thread_id = i;
-        thread_data[i].start_index = i * users_per_thread;
-        thread_data[i].end_index = (i + 1) * users_per_thread;
-        if (i == THREADS_USERS - 1) {
-            thread_data[i].end_index = num_users;
-        }
-
-        pthread_create(&threads[i], NULL, insert_users, (void*)&thread_data[i]);
-    }
-
-    for (int i = 0; i < THREADS_USERS; ++i) {
-        pthread_join(threads[i], NULL);
-    }
-
-    int total_errors = 0;
-    double total_work_time = 0.0;
-    double total_lock_time = 0.0;
-    for (int i = 0; i < THREADS_USERS; ++i) {
-        total_errors += thread_data[i].errors;
-        total_work_time += thread_data[i].work_time;
-        total_lock_time += thread_data[i].lock_time;
-    }
-
-    printf("Inserción de usuarios completada con %d errores.\n", total_errors);
-    printf("Tiempo total de trabajo: %.2f milisegundos\n", total_work_time);
-    printf("Tiempo total en seccion critica (con mutex): %.2f milisegundos\n", total_lock_time);
-}
-
-void read_json_file(char* filename, struct user_data* users, int* num_users) {
+void read_json_file(char* filename) {
     FILE *file = fopen(filename, "r");
     if (!file) {
         perror("Open File");
@@ -144,52 +55,154 @@ void read_json_file(char* filename, struct user_data* users, int* num_users) {
 
     cJSON *json = cJSON_Parse(buffer);
     if (json == NULL) {
-        perror("Parse JSON");
+        fprintf(stderr, "Error parsing JSON: %s\n", cJSON_GetErrorPtr());
         free(buffer);
         return;
     }
 
     if (!cJSON_IsArray(json)) {
-        perror("JSON is not Array");
+        fprintf(stderr, "JSON is not an array\n");
         cJSON_Delete(json);
         free(buffer);
         return;
     }
 
-    *num_users = cJSON_GetArraySize(json);
-    if (*num_users > MAX_USERS) {
-        perror("Exceeded maximum users");
+    // Arrays para controlar números de cuenta duplicados
+    int *account_numbers = (int *)malloc(cJSON_GetArraySize(json) * sizeof(int));
+    if (account_numbers == NULL) {
+        perror("Allocate account_numbers");
         cJSON_Delete(json);
         free(buffer);
-        exit(EXIT_FAILURE);
+        return;
     }
 
-    int user_count = 0;
-    for (int i = 0; i < *num_users; ++i) {
-        cJSON *item = cJSON_GetArrayItem(json, i);
-        cJSON *account_number = cJSON_GetObjectItem(item, "no_cuenta");
-        cJSON *full_name = cJSON_GetObjectItem(item, "nombre");
-        cJSON *saldo = cJSON_GetObjectItem(item, "saldo");
+    memset(account_numbers, 0, cJSON_GetArraySize(json) * sizeof(int));
 
-        if (!cJSON_IsNumber(account_number) || !cJSON_IsString(full_name) || !cJSON_IsNumber(saldo)) {
-            snprintf(errors[num_errors].error_message, sizeof(errors[num_errors].error_message), "Invalid JSON format at line %d", i + 1);
+    int user_count = 0;
+    for (int i = 0; i < cJSON_GetArraySize(json); ++i) {
+        cJSON *item = cJSON_GetArrayItem(json, i);
+
+        // Validar que 'item' sea un objeto JSON válido
+        if (!cJSON_IsObject(item)) {
+            snprintf(errors[num_errors].error_message, sizeof(errors[num_errors].error_message), "Error en el formato del objeto JSON en el registro %d: no es un objeto JSON válido", i + 1);
             errors[num_errors].line = i + 1;
             num_errors++;
             continue; // Omitir el registro con error y continuar con el siguiente
         }
 
+        cJSON *account_number = cJSON_GetObjectItem(item, "no_cuenta");
+        cJSON *full_name = cJSON_GetObjectItem(item, "nombre");
+        cJSON *saldo = cJSON_GetObjectItem(item, "saldo");
+
+        // Validar la existencia y tipo correcto de los campos
+        if (account_number == NULL || !cJSON_IsNumber(account_number)) {
+            snprintf(errors[num_errors].error_message, sizeof(errors[num_errors].error_message), "Error en el campo 'no_cuenta' en el registro %d: se espera un número de cuenta válido", i + 1);
+            errors[num_errors].line = i + 1;
+            num_errors++;
+            continue; // Omitir el registro con error y continuar con el siguiente
+        }
+
+        if (full_name == NULL || !cJSON_IsString(full_name)) {
+            snprintf(errors[num_errors].error_message, sizeof(errors[num_errors].error_message), "Error en el campo 'nombre' en el registro %d: se espera un nombre válido", i + 1);
+            errors[num_errors].line = i + 1;
+            num_errors++;
+            continue; // Omitir el registro con error y continuar con el siguiente
+        }
+
+        if (saldo == NULL || !cJSON_IsNumber(saldo) || saldo->valuedouble < 0) {
+            snprintf(errors[num_errors].error_message, sizeof(errors[num_errors].error_message), "Error en el campo 'saldo' en el registro %d: se espera un saldo válido y positivo", i + 1);
+            errors[num_errors].line = i + 1;
+            num_errors++;
+            continue; // Omitir el registro con error y continuar con el siguiente
+        }
+
+        // Validar número de cuenta duplicado
+        if (account_numbers[account_number->valueint] != 0) {
+            snprintf(errors[num_errors].error_message, sizeof(errors[num_errors].error_message), "Error en el registro %d: número de cuenta duplicado", i + 1);
+            errors[num_errors].line = i + 1;
+            num_errors++;
+            continue; // Omitir el registro con error y continuar con el siguiente
+        }
+
+        account_numbers[account_number->valueint] = 1; // Marcar número de cuenta como utilizado
+
+        // Almacenar los datos válidos en la estructura de usuarios
         users[user_count].account_number = account_number->valueint;
         strncpy(users[user_count].full_name, full_name->valuestring, sizeof(users[user_count].full_name) - 1);
-        users[user_count].full_name[sizeof(users[user_count].full_name) - 1] = '\0'; // Ensure null-terminated
+        users[user_count].full_name[sizeof(users[user_count].full_name) - 1] = '\0'; // Asegurar que esté terminado en nulo
         users[user_count].saldo = saldo->valuedouble;
         users[user_count].line = i + 1;
         user_count++;
-        printf("Usuario %d: %d, %s, %.2f\n", user_count, users[user_count - 1].account_number, users[user_count - 1].full_name, users[user_count - 1].saldo);
+
+        // Contar usuarios por hilo
+        if (user_count % 3 == 1) {
+            usuariosHilo1++;
+        } else if (user_count % 3 == 2) {
+            usuariosHilo2++;
+        } else {
+            usuariosHilo3++;
+        }
     }
 
-    *num_users = user_count;
+    num_users = user_count;
+
     cJSON_Delete(json);
     free(buffer);
+    free(account_numbers); // Liberar la memoria utilizada por account_numbers
+}
+
+
+void generate_report() {
+    char nombre[100];
+    char fecha[100];
+    time_t tiempo_actual;
+    struct tm *info_tiempo;
+    time(&tiempo_actual);
+    info_tiempo = localtime(&tiempo_actual);
+    strftime(nombre, sizeof(nombre), "carga_%Y_%m_%d-%H_%M_%S.log", info_tiempo);
+    strftime(fecha, sizeof(fecha), "%Y-%m-%d %H:%M:%S", info_tiempo);
+    FILE *reporte;
+    reporte = fopen(nombre, "w");
+
+    if (reporte == NULL) {
+        printf("Error al generar el archivo de reportes de usuario.\n");
+        return;
+    }
+
+    fprintf(reporte, "------------------------------------------CARGA DE USUARIOS------------------------------------------\n");
+    fprintf(reporte, "Fecha: %s\n", fecha);
+    fprintf(reporte, "Usuarios Cargados:\n");
+    fprintf(reporte, "Hilo #1: %d\n", usuariosHilo1);
+    fprintf(reporte, "Hilo #2: %d\n", usuariosHilo2);
+    fprintf(reporte, "Hilo #3: %d\n", usuariosHilo3);
+    fprintf(reporte, "Total: %d\n", num_users);
+    fprintf(reporte, "Errores:\n");
+    
+    if (num_errors == 0) {
+        fprintf(reporte, "No se encontraron errores durante la carga de usuarios.\n");
+    } else {
+        for (int i = 0; i < num_errors; ++i) {
+            fprintf(reporte, "-\n");
+            fprintf(reporte, "Linea #%d: %s\n", errors[i].line, errors[i].error_message);
+        }
+    }
+
+    fclose(reporte);
+    printf("Reporte generado: %s\n", nombre);
+}
+
+
+
+void print_users() {
+    printf("Usuarios cargados:\n");
+    for (int i = 0; i < num_users; ++i) {
+        printf("Usuario %d:\n", i + 1);
+        printf("Número de cuenta: %d\n", users[i].account_number);
+        printf("Nombre completo: %s\n", users[i].full_name);
+        printf("Saldo: %.2f\n", users[i].saldo);
+        printf("Línea en JSON: %d\n", users[i].line);
+        printf("\n");
+    }
 }
 
 void print_error_report() {
@@ -204,8 +217,8 @@ void print_error_report() {
 }
 
 int main() {
-    headerUsers = NULL;
-    
+    char route_users[200];
+
     printf("*******************************\n");
     printf("|PRACTICA 2                   |\n");
     printf("|Sistemas Operativos2         |\n");
@@ -213,17 +226,8 @@ int main() {
     printf("|Eduardo Agustin | 201801627  |\n");
     printf("*******************************\n\n\n");
 
-    while(true){
-        pthread_mutex_init(&mutex, NULL);
-        pthread_t threads[3];
-        ThreadData thread_data[3];
-        pthread_t threadsT[4];
-        ThreadData thread_dataT[4];
-        int lines_per_thread = MAX_USERS / 3;
-        int lines_per_threadT = MAX_TRANSACIONES / 4;
+    while (1) {
         char option;
-        char route_users[2000];
-        char route_operations[2000];
         printf("|***********************MENU***********************|\n");
         printf("|1.Cargar usuarios                                 |\n");
         printf("|2.Cargar operaciones                              |\n");
@@ -232,35 +236,26 @@ int main() {
         printf("|5.Salir                                           |\n");
         printf("|**************************************************|\n");
 
-        scanf("%c", &option);
-        getchar(); //
+        scanf(" %c", &option);
 
-        switch(option){
+        switch (option) {
             case '1':
                 printf("Ingrese la ruta del archivo de usuarios: ");
                 scanf("%s", route_users);
                 getchar();
-                read_json_file(route_users, users, &num_users);
+                read_json_file(route_users);
                 print_error_report();
-
-                clock_t start_time = clock(); // Inicio del contador
-                execute_concurrent_user_insertions();
-
-                clock_t end_time = clock(); // Fin del contador
-                double time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC *1000;
-                printf("Tiempo de ejecución: %.2f milisegundos\n", time_spent);
+                print_users();
+                generate_report(); // Generar reporte al finalizar la carga
                 break;
             case '2':
-                printf("Ingrese la ruta del archivo de operaciones: ");
-                // Aquí debes agregar el código para manejar la carga de operaciones
+                printf("Cargar operaciones\n");
                 break;
             case '3':
                 printf("Operaciones individuales\n");
-                // Aquí debes agregar el código para manejar las operaciones individuales
                 break;
             case '4':
                 printf("Generar estados de cuenta\n");
-                // Aquí debes agregar el código para generar estados de cuenta
                 break;
             case '5':
                 printf("Saliendo...\n");
@@ -270,7 +265,7 @@ int main() {
                 break;
         }
     }
+
     return 0;
 }
 
-//gcc main.c users.c -o main -lcjson
